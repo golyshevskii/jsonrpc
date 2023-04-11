04d3a38403e47da7e6ac9e99e64434f36dda46ec
slava golyshevskii <73784126+golyshevskii@users.noreply.github.com>
2023-04-11 13:41:25 +0400

    тестирование

---
+++ tests.py
@@ -0,0 +1,50 @@
+from collections import deque
+from datetime import datetime
+
+
+class Connection:
+    _connection = (None, None)
+    _timeout = None
+    _timeout_state = deque(maxlen=2)
+
+    def make_connection(self, host):
+        self._timeout_state.append(self._timeout)
+        if host == self._connection[0]:
+            if len(set(self._timeout_state)) <= 1:
+                self.reset_timeout()
+                return self._connection[1]
+
+        self._connection = host, f'connection {host} -> {self._timeout}'
+        self.reset_timeout()
+        return self._connection[1]
+
+    def set_timeout(self, timeout):
+        try:
+            self._timeout = float(timeout)
+        except (ValueError, TypeError, Exception) as ex:
+            print(f'TransportMixIn > set_timeout: ERROR -> {datetime.now()}\n{ex.__class__.__name__}:\n{ex}')
+            raise Exception('Incorrect timeout value. Should be a number.')
+
+    def reset_timeout(self):
+        self._timeout = None
+
+
+tests = (
+    ('http_1', None),
+    ('http_1', 60),
+    ('http_1', None),
+    ('http_1', 30),
+    ('http_1', 20),
+    ('http_2', 100),
+    ('http_2', None),
+    ('http_2', None),
+    ('http_2', 30)
+)
+
+conn = Connection()
+for test in tests:
+    host, timeout = test[0], test[1]
+    if timeout:
+        conn.set_timeout(timeout)
+    print(f'TEST ({host}, {timeout}) -> {datetime.now()}')
+    print(f'response > {conn.make_connection(host)}\nattributes > {conn.__dict__}')
